04c2dc3f8fa8ffd97205a7126f7b77590c8b4b3b
Théo-toto Certa <totoc.pro@gmail.com>
2025-11-20 12:21:08 +0100

    feat: add dialog, popover and navigation UI components

---
+++ src/components/ui/popover/PopoverTrigger.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { PopoverTrigger, type PopoverTriggerProps } from 'reka-ui'
+import { computed, type HTMLAttributes } from 'vue'
+
+const props = defineProps<PopoverTriggerProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+  return delegated
+})
+</script>
+
+<template>
+  <PopoverTrigger v-bind="delegatedProps" :class="props.class">
+    <slot />
+  </PopoverTrigger>
+</template>
